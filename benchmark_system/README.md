18bd0818954889d9b42ea4231346fddadffe737c
Cynthia Van Hee <cynthia.vanhee@ugent.be>
2017-09-11 14:44:27 +0200

    Adding SemEval-2018 Task 3 benchmark system

---
+++ benchmark_system/README.md
@@ -0,0 +1,25 @@
+---
+# SemEval18 Irony Shared Task Example Scripts
+This repository contains a benchmark system for the SemEval-2018 Task 3 <b>Irony detection in English tweets</b>.
+The script <tt>example.py</tt> contains Python example code for simple featurisation and classification using the Scikit-learn library.
+
+The script is written in Python 3 but is Python 2 compatible.
+
+
+## Dependencies
+- Python 3.6.x
+- NLTK 3.2.4
+- numpy 1.13.1
+- scikit-learn 0.19.0
+- scipy 0.19.1
+- six 1.10.0
+
+Python module dependencies can be found in `requirements.txt`
+
+Install with pip:
+`pip install -r requirements.txt`
+
+## Usage
+Run `python example.py` from terminal.
+
+**NOTE**: the path to the input file and the task (A or B) have to be hard-coded in the script.
---
+++ benchmark_system/example.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+
+'''
+example.py
+
+Benchmark system for the SemEval-2018 Task 3 on Irony detection in English tweets.
+The system makes use of token unigrams as features and outputs cross-validated F1-score.
+
+Date: 1/09/2017
+Copyright (c) Gilles Jacobs & Cynthia Van Hee, LT3. All rights reserved.
+'''
+
+from nltk.tokenize import TweetTokenizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import cross_val_score, cross_val_predict
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import LinearSVC
+from sklearn.datasets import dump_svmlight_file
+from sklearn import metrics
+import numpy as np
+import logging
+import codecs
+
+
+logging.basicConfig(level=logging.INFO)
+
+def parse_dataset(fp):
+    '''
+    Loads the dataset .txt file with label-tweet on each line and parses the dataset.
+    :param fp: filepath of dataset
+    :return:
+        corpus: list of tweet strings of each tweet.
+        y: list of labels
+    '''
+    y = []
+    corpus = []
+    with open(fp, 'rt') as data_in:
+        for line in data_in:
+            if not line.startswith("Tweet index"): # discard first line if it contains metadata
+                line = line.rstrip() # remove trailing whitespace
+                label = int(line.split("\t")[1])
+                tweet = line.split("\t")[2]
+                y.append(label)
+                corpus.append(tweet)
+
+    return corpus, y
+
+
+def featurize(corpus):
+    '''
+    Tokenizes and creates TF-IDF BoW vectors.
+    :param corpus: A list of strings each string representing document.
+    :return: X: A sparse csr matrix of TFIDF-weigted ngram counts.
+    '''
+
+    tokenizer = TweetTokenizer(preserve_case=False, reduce_len=True, strip_handles=True).tokenize
+    vectorizer = TfidfVectorizer(strip_accents="unicode", analyzer="word", tokenizer=tokenizer, stop_words="english")
+    X = vectorizer.fit_transform(corpus)
+    # print(vectorizer.get_feature_names()) # to manually check if the tokens are reasonable
+    return X
+
+
+
+if __name__ == "__main__":
+    # Experiment settings
+
+    # Dataset: SemEval2018-T4-train-taskA.txt or SemEval2018-T4-train-taskB.txt
+    DATASET_FP = "./SemEval2018-T4-train-taskA.txt"
+    PREDICTIONSFILE = open("./predictions.txt", "w")
+    TASK = "A" # Define, A or B
+
+    K_FOLDS = 10 # 10-fold crossvalidation
+    CLF = LinearSVC() # the default, non-parameter optimized linear-kernel SVM
+
+    # Loading dataset and featurised simple Tfidf-BoW model
+    corpus, y = parse_dataset(DATASET_FP)
+    X = featurize(corpus)
+
+    class_counts = np.asarray(np.unique(y, return_counts=True)).T.tolist()
+
+    # Returns an array of the same size as 'y' where each entry is a prediction obtained by cross validated
+    predicted = cross_val_predict(CLF, X, y, cv=K_FOLDS)
+    
+    # Change F1-score calculation depending on the task
+    if TASK.lower() == 'a':
+        score = metrics.f1_score(y, predicted, pos_label=1)
+    elif TASK.lower() == 'b':
+        score = metrics.f1_score(y, predicted, average="macro")
+    score = metrics.f1_score(y, predicted)
+    print ("F1-score Task", TASK, score)
+    for p in predicted:
+        PREDICTIONSFILE.write("{}\n".format(p))
+    PREDICTIONSFILE.close()
+    
+    
+    
